fe763185946be83b20da626605319733bd7f97cb
Bartlomiej Plotka <bwplotka@gmail.com>
2022-02-25 18:52:07 +0000

    Added tracing to profile package.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- performance/pkg/profiles/profile.go
+++ performance/pkg/profiles/profile.go
@@ -7,13 +7,38 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"runtime/pprof"
+	"runtime/trace"
 
 	"github.com/efficientgo/tools/core/pkg/errcapture"
 	"github.com/felixge/fgprof"
 	"github.com/pkg/errors"
 )
 
+// StopHeapRecording stops heap recording, which will mean that
+// allocation after this function will be not recorded in any heap profile
+// until we resume it.
+// NOTE: Given the runtime implementation this setting is global.
+func StopHeapRecording() {
+	runtime.MemProfileRate = 0
+}
+
+// StartHeapRecording is setting memory profile rate to default value of
+// profiler sample every 512 KB allocated.
+// If `everyAlloc` is true it sets profile rate to sample for every byte allocated.
+// NOTE: Given the runtime implementation this setting is global.
+func StartHeapRecording(everyAlloc bool) {
+	if everyAlloc {
+		runtime.MemProfileRate = 1
+		return
+	}
+	runtime.MemProfileRate = 512 * 1024
+}
+
+// Heap creates a heap profile in mem.pprof file in given directory.
+// Directory might be non-existent, heap will create it if needed.
+// NOTE: Given the runtime implementation this setting is global.
 func Heap(dir string) (err error) {
 	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
 		return err
@@ -35,9 +60,10 @@ const (
 	CPUTypeFGProf CPUType = "fgprof"
 )
 
-// StartCPU starts CPU profiling. If no error, it returns close function that stops and flushes profile to provided
-// directory.
-// NOTE(bwplotka): It does not make sense to run more than one of those.
+// StartCPU starts CPU profiling. If no error is returned, it returns close function that stops and flushes
+// profile to cpu.pprof or cpu.fgprof.pprof file in a given directory.
+// Directory might be non-existent, heap will create it if needed.
+// NOTE: Given the runtime implementation this setting is global.
 func StartCPU(dir string, typ CPUType) (closeFn func() error, err error) {
 	fileName := "cpu.pprof"
 	switch typ {
@@ -75,4 +101,29 @@ func StartCPU(dir string, typ CPUType) (closeFn func() error, err error) {
 		}
 	}
 	return closeFn, nil
+}
+
+// StartTrace starts tracingIf no error is returned, it returns close function that stops and flushes
+// profile to trace.out file in a given directory.
+// Directory might be non-existent, heap will create it if needed.
+// NOTE: Given the runtime implementation this setting is global.
+func StartTrace(dir string) (closeFn func() error, err error) {
+	fileName := "trace.out"
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	f, err := os.Create(filepath.Join(dir, fileName))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := trace.Start(f); err != nil {
+		return nil, err
+	}
+	return func() error {
+		trace.Stop()
+		return errors.Wrapf(f.Close(), "close %v", filepath.Join(dir, fileName))
+	}, nil
 }
