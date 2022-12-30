3ffd7302f304839a43a3d26129060695e8b6b1e5
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-12-30 16:43:04 +0100

    Add copyright header

--- extkingpin/pathorcontent_reloader_test.go
+++ extkingpin/pathorcontent_reloader_test.go
@@ -1,3 +1,11 @@
+// Copyright (c) The EfficientGo Authors.
+// Licensed under the Apache License 2.0.
+
+// Taken from Thanos project.
+//
+// Copyright (c) The Thanos Authors.
+// Licensed under the Apache License 2.0.
+
 package extkingpin
 
 import (
